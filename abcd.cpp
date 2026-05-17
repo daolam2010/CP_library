0642f701019950d5e4ab311bcae5aa54423c8b39
daolam2010 <daolam2010@gmail.com>
2026-05-17 22:02:44 +0700

    Add initial implementation of algorithm in abcd.cpp

---
+++ abcd.cpp
@@ -0,0 +1,87 @@
+//tim cap a - b = =c - d
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+#define int long long
+
+signed main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    map<int,int> mp;
+
+    int ans = 0;
+
+    for (int p = 3; p <= n; p++) {
+
+        // thêm các cặp bên trái
+        int j = p - 1;
+
+        for (int i = 1; i < j; i++) {
+            mp[a[i] - a[j]]++;
+        }
+
+        // xét cặp bên phải
+        for (int q = p + 1; q <= n; q++) {
+            ans += mp[a[p] - a[q]];
+        }
+    }
+
+    cout << ans;
+}
